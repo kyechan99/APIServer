da2d35a3ad4b4efc18f964ca1f9a6d0da02b2e15
VallistA <vallista@tistory.com>
2017-06-14 21:22:06 +0900

    project upload

---
+++ API-Server/path.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.corePath = () => {
+    return '../../Core/Core/Core';
+};
---
+++ API-Server/routes/board.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var core = require('../path');
+var boardService = require(core + '/services/board');
+
+router.get('/categories/list', function(req, res) {
+    var promise = boardService.getCategories();
+
+    Promise.all([promise])
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
+});
+
+router.post('/board/post', function(req, res) {
+    var promise = boardService.post(req);
+
+    Promise.all([promise])
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
+});
+
+module.exports = router;
